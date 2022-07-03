d5f2ff52f04cf6a41e89f6a5017b55e9280ad35f
MyClass-2 <3010006821@qq.com>
2022-07-03 17:02:44 +0800

    新增LeetCodeMain

---
+++ src/com/fzf/sfp/leetcode/LeetCodeMain.java
@@ -0,0 +1,11 @@
+package com.fzf.sfp.leetcode;
+
+/**
+ * @author fanZhiFei
+ * @create 2022/7/3
+ */
+public class LeetCodeMain {
+    public static void main(String[] args) {
+        System.out.println(1);
+    }
+}
