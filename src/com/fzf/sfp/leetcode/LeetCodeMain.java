137177a501258ae4815ef6a2d4ebddc5a82b95dd
MyClass-2 <3010006821@qq.com>
2022-07-03 17:56:21 +0800

    测试

--- src/com/fzf/sfp/leetcode/LeetCodeMain.java
+++ src/com/fzf/sfp/leetcode/LeetCodeMain.java
@@ -6,7 +6,6 @@ package com.fzf.sfp.leetcode;
  */
 public class LeetCodeMain {
     public static void main(String[] args) {
-        System.out.println(2);
         System.out.println(1);
     }
 }
